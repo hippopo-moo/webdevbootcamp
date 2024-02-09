9053a8451d314562aa7996a59ae020df8de69113
ykosugi <hippopo.moo@gmail.com>
2024-02-09 13:52:16 +0900

    bootstrapのscriptを静的ファイルに切り出し

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // routeの設定
 app.use('/campgrounds', campgroundRouter)
